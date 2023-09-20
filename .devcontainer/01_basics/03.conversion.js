3b7e4d7672fc052a6c4e83cb5734a16a819118e7
Joshi Darshi <103184906+JoshiDarshi@users.noreply.github.com>
2023-09-20 09:59:00 +0000

    complex conditions

--- .devcontainer/01_basics/03.conversion.js
+++ .devcontainer/01_basics/03.conversion.js
@@ -1,22 +1,44 @@
-let score = "4323243eddfdd"
-console.log(typeof score);
-let value = Number(score)
-console.log(typeof value);
-console.log(value);
+// let score = "4323243eddfdd"
+// console.log(typeof score);
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
 
 //"33" => 33
 //"33abc" => NAN
 //true => 1
-let islogin  = 1
-let updatelogin = Boolean(islogin)
-console.log(updatelogin);
+// let islogin  = 1
+// let updatelogin = Boolean(islogin)
+// console.log(updatelogin);
 //1 => true
 //0 => false
 //"" => false
 //"darshi" => true
 
 //number to string
-let num = 1199
-let str = String(num)
-console.log(str);
-console.log(typeof str);
+// let num = 1199
+// let str = String(num)
+// console.log(str);
+// console.log(typeof str);
+
+//***************operations *************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(typeof negValue);
+
+let str1 = "hello"
+let str2 = " darshi"
+let str3 = str1 + str2
+console.log(str3);
+//complex situation
+console.log(typeof 1 + "2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let gamecounter = 100
+gamecounter++
+--gamecounter
+console.log(gamecounter);
