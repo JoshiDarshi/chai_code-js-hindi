e96df0397ef6ddb5a20f5603468b0004fc5c7fd3
Joshi Darshi <103184906+JoshiDarshi@users.noreply.github.com>
2023-09-20 12:02:43 +0000

    more detailed of datatypes with memory concept

--- .devcontainer/01_basics/datatype_summary.js
+++ .devcontainer/01_basics/datatype_summary.js
@@ -8,3 +8,24 @@ let id1 = Symbol("123")
 let id2 = Symbol("123")
 console.log(id1==id2);
 console.log(id1===id2);
+
+/****************Memory **********************/
+
+
+//primitive type uses Stack Memory
+//ex:-
+let video = "youtube"
+let v2 = video
+v2 = "insta"
+console.log(video);
+console.log(v2); 
+
+//Reference type uses Heap Memory
+//ex
+let obj1 = {
+    name : 'abc',
+    age : 200
+}
+let obj2 = obj1
+obj2.age=20
+console.log(obj2);
