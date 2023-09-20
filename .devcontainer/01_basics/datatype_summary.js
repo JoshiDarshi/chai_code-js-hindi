3651f1ec6a82f636ade256ea87eb851ac3a893b2
Joshi Darshi <103184906+JoshiDarshi@users.noreply.github.com>
2023-09-20 10:28:07 +0000

    more detailed of datatype

---
+++ .devcontainer/01_basics/datatype_summary.js
@@ -0,0 +1,10 @@
+//Primitive datatype  --> Number, Null, String ,Symbol, Boolean , Bigint , Undefined
+
+//Reference datatype or Non Primitive datatype --> array , object , functions
+//js is dynamically typed language hame usse ye nhi batana pdata ki ye int num = 100 ham directly let num = 100 likh sakte hai
+
+//Symbol syntax
+let id1 = Symbol("123")
+let id2 = Symbol("123")
+console.log(id1==id2);
+console.log(id1===id2);
