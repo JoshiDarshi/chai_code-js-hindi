cbcab51ef1badc49d79b910373df4fbf620f7207
Joshi Darshi <103184906+JoshiDarshi@users.noreply.github.com>
2023-09-20 06:01:30 +0000

    datatypes

--- .devcontainer/01_basics/01.variable.js
+++ .devcontainer/01_basics/01.variable.js
@@ -1,6 +1,6 @@
 const accId = 1234
 let accName = "efmddkds"
 let accEmail = "sdas@gmail.com"
-let accCity = "fdfdfd"
+ accCity = "fdfdfd"
 console.log(accId)
 console.table([accId,accName,accEmail,accCity])
---
+++ .devcontainer/01_basics/02.datatype.js
@@ -0,0 +1,12 @@
+"use strict" //treat all js code as new version 
+//alert(3+4) it will run on browser
+console.log(4+6)
+console.log("darshi")
+//datatypes
+// number =1232
+// string = "erterf"
+// boolean = true
+// null
+// undefined
+// bigint
+// symbol
