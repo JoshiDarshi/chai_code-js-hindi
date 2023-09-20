817de7e2a3ff2bb7aa9e4241a06aa7f601d13689
Joshi Darshi <103184906+JoshiDarshi@users.noreply.github.com>
2023-09-20 10:13:38 +0000

    comparing datatype

---
+++ .devcontainer/01_basics/04.comparision.js
@@ -0,0 +1,13 @@
+console.log("2" < 10);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+//=== strict checking it check with datatype also
+console.log("2"===2);
