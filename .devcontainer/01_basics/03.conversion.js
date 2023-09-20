0d902d72cf678653286b2ea6ec28553ec6cf87b7
Joshi Darshi <103184906+JoshiDarshi@users.noreply.github.com>
2023-09-20 09:36:27 +0000

    conversion in datatype

---
+++ .devcontainer/01_basics/03.conversion.js
@@ -0,0 +1,22 @@
+let score = "4323243eddfdd"
+console.log(typeof score);
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+//"33" => 33
+//"33abc" => NAN
+//true => 1
+let islogin  = 1
+let updatelogin = Boolean(islogin)
+console.log(updatelogin);
+//1 => true
+//0 => false
+//"" => false
+//"darshi" => true
+
+//number to string
+let num = 1199
+let str = String(num)
+console.log(str);
+console.log(typeof str);
